d533a03a4f680238cd4c1a87b322b0c455198cac
Tobias Steidle <tobias.steidle@softwaredev.de>
2020-03-21 10:42:14 +0100

    feat: intitial backup

---
+++ src/main/java/de/wirvsvirus/hack/virtuellestimmungsringe/VirtuelleStimmungsringeApplication.java
@@ -0,0 +1,13 @@
+package de.wirvsvirus.hack.virtuellestimmungsringe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtuelleStimmungsringeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtuelleStimmungsringeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/wirvsvirus/hack/virtuellestimmungsringe/VirtuelleStimmungsringeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wirvsvirus.hack.virtuellestimmungsringe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtuelleStimmungsringeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
